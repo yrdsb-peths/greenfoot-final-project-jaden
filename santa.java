6ab662621504ace04aa93e4786725d1cd29c1603
JadenChan2005 <101139011+JadenChan2005@users.noreply.github.com>
2022-06-02 14:17:49 -0400

    animated santas

--- santa.java
+++ santa.java
@@ -8,30 +8,81 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class santa extends Actor
 {
-    GreenfootImage[] santaDie = new GreenfootImage[17];
+    GreenfootImage[] santaWalkRight = new GreenfootImage[11];
+    GreenfootImage[] santaWalkLeft = new GreenfootImage[11];
     
+    
+    
+    SimpleTimer animationTimer = new SimpleTimer();
+    String facing = "right";
     public santa()
     {
-        for(int i = 1; i < santaDie.length; i++)
+        for(int i = 1; i < santaWalkRight.length; i++)
         {
-            santaDie[i] = new GreenfootImage("images/santa/Dead (" + i + ").png");
+            GreenfootImage image = new GreenfootImage("images/santa/Run (" + i + ").png");
+            image.scale(200, 200);
+            santaWalkRight[i] = image;
         }
-        setImage(santaDie[15]);
-        GreenfootImage santaDie = getImage();
-        santaDie.scale(200, 200);
-        setImage (santaDie);
+        
+        for(int i = 1; i < santaWalkLeft.length; i++)
+        {
+            GreenfootImage image = new GreenfootImage("images/santa/Run (" + i + ").png");
+            image.scale(200, 200);
+            santaWalkLeft[i] = image;
+            santaWalkLeft[i].mirrorHorizontally();
+        }
+        animationTimer.mark();
+        
+        setImage(santaWalkRight[0]);
     }
+    int imageIndex = 0;
     /**
      * Act - do whatever the santa wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public void animateSantaWalk()
+    {
+        if(animationTimer.millisElapsed() < 300)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(santaWalkRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % santaWalkRight.length;
+        }
+        else
+        {
+            setImage(santaWalkLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % santaWalkLeft.length;
+        }
+    }
     public void act()
     {
-        move(5);
-        move(-5);
-        setLocation(getX(), getY()-5);
-        setLocation(getX(), getY()+5);
-        turn(2);
+        if(Greenfoot.isKeyDown("a")){
+            facing = "left";
+            move(-5);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            facing = "right";
+            move(5);
+        }
+        if(Greenfoot.isKeyDown("w")){
+            setLocation(getX(), getY()-5);
+        }
+        if(Greenfoot.isKeyDown("s")){
+            setLocation(getX(), getY()+5);
+        }
+        if(Greenfoot.isKeyDown("space")){
+            turn(2);
+        }
+        
+        
+        //animate santa
+        animateSantaWalk();
+            
     }
 
 }
