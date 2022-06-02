6cacb0ec7f2fcf0e66999ffd679d5f7fee59f37b
rainaru2 <335691838@gapps.yrdsb.ca>
2022-06-02 13:29:40 -0400

    yeti change

--- PinkYeti.java
+++ PinkYeti.java
@@ -12,8 +12,8 @@ public class PinkYeti extends Actor
      * Act - do whatever the PinkYeti wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] move = new GreenfootImage[4];
-    
+    GreenfootImage[] move = new GreenfootImage[4];    
+    SimpleTimer animation = new SimpleTimer();
     public PinkYeti()
     {
         for(int i = 0; i < 4; i++)
@@ -21,8 +21,19 @@ public class PinkYeti extends Actor
             move[i] = new GreenfootImage("images/pinkyetis/p.move" + i + ".png");
         }
         setImage(move[0]);
+        animation.mark();
     }
-    
+    int imageIndex = 0;
+    public void animate()
+    {
+        if(animation.millisElapsed() < 500)
+        {
+            return;
+        }
+        animation.mark();
+        setImage(move[imageIndex]);
+        imageIndex = (imageIndex + 1) % move.length;
+    }
     public void act()
     {
         // Add your action code here.
