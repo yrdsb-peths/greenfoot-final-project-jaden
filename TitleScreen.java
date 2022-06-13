c78cba92d01ce891fe70310ae5492bcc188b30e7
rainaru2 <335691838@gapps.yrdsb.ca>
2022-06-12 22:40:28 -0400

    changed background

--- TitleScreen.java
+++ TitleScreen.java
@@ -18,8 +18,8 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, 180);
-        addObject(start, getWidth()/2,250);
+        addObject(titleLabel, getWidth()/2, 100);
+        addObject(start, getWidth()/2,350);
     }
     public void act()
     {
