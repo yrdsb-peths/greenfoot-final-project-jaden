dadc0927c8c76dd492d9a59e50b6c5c5597c1e93
rainaru2 <335691838@gapps.yrdsb.ca>
2022-06-14 13:54:23 -0400

    chose penguin sprites for title screen

---
+++ titlePenguin.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class titlePenguin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class titlePenguin extends Actor
+{
+    /**
+     * Act - do whatever the titlePeng wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    GreenfootImage[] walk = new GreenfootImage[9];
+    SimpleTimer animation = new SimpleTimer();
+    public void titlePenguin()
+    {
+        for(int i = 0; i < walk.length; i++)
+        {
+            walk[i] = new GreenfootImage("images/titlePengs/walk" + i + ".png"); 
+            
+        }
+        setImage(walk[0]);
+        animation.mark();
+    }
+    int imageIndex = 0;
+    public void animate()
+    {
+        if(animation.millisElapsed() < 500)
+        {
+            return;
+        }
+        animation.mark();
+        setImage(walk[imageIndex]);
+        imageIndex = (imageIndex + 1) % walk.length;
+    }
+    public void act()
+    {
+        animate();
+    }
+    
+}
